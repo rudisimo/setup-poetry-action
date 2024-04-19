18a5ed5276e748269c345f368a9b4b6eb7e93e8e
Rodolfo Puig <rodolfo@puig.io>
2024-04-19 10:35:08 -0400

    github: more installer debugging

--- scripts/install-poetry-wrapper.sh
+++ scripts/install-poetry-wrapper.sh
@@ -11,10 +11,16 @@ POETRY_VERSION=$2
 
 # Download installer script
 POETRY_INSTALLER="$(mktemp)"
-trap "rm -f ${POETRY_INSTALLER}" ERR EXIT
 curl -sSL https://install.python-poetry.org --output "${POETRY_INSTALLER}"
 
-# Build installer arguments
+# Error handling
+cleanup() {
+    echo >&2 "ERROR. Checking installer logs..."
+    cat >&2 $GITHUB_WORKSPACE/poetry-installer-error-*.log || true
+}
+trap cleanup ERR
+
+# Generate installer arguments
 POETRY_INSTALL_ARGS=()
 if [ -n "${POETRY_VERSION}" ]; then
     if [[ "${POETRY_VERSION}" =~ ^preview ]]; then
@@ -26,10 +32,15 @@ if [ -n "${POETRY_VERSION}" ]; then
     fi
 fi
 
-# Install Poetry
-export POETRY_HOME="${HOME}/.local/share/poetry"
-python "${POETRY_INSTALLER}" ${POETRY_INSTALL_ARGS[@]}
+# Generate poetry directories
+case "${POETRY_OS}" in
+    Windows*) POETRY_HOME="${APPDATA}\poetry" ;;
+    *)        POETRY_HOME="${HOME}/.local/share/poetry";;
+esac
 POETRY_BIN_DIR="${POETRY_HOME}/bin"
+
+# Install Poetry
+POETRY_HOME=$POETRY_HOME python "${POETRY_INSTALLER}" ${POETRY_INSTALL_ARGS[@]}
 
 # Configure exports
 echo "binary=${POETRY_BIN_DIR}/poetry" >> $GITHUB_OUTPUT
